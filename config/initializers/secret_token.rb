67bb0da8e2ff8061ffc530abf17e3ea252c452bd
mooreryan <moorer@udel.edu>
2013-07-30 10:25:38 -0400

    Initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+KmerBlast::Application.config.secret_key_base = 'ffed73a45c59d6a95268e2661bfe0925852ccb4e14d0bb659e4e146be0bcf00403e1f0827888a0a59300d4d441b141cc09f6a29440e53e9286adfde97dcea13c'
